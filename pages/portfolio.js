1d8d9225937da8019ba4721c92f6ff98f77d70d7
Aloysius Teo <aloysiusteo72@gmail.com>
2022-12-10 17:10:12 -0800

    Update portfolio.js

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -5,6 +5,7 @@ import SEO from "../components/seo";
 //import Video from "../components/video";
 //import Testimonials from "../components/testimonials";
 
+import Subscriptions from "../components/dashboard/subscription";
 import LayoutDashboard from "../layouts/layoutDashboard";
 
 //import CardLineChart from "../components/dashboard/cards/cardLineChart.js";
@@ -15,34 +16,15 @@ import LayoutDashboard from "../layouts/layoutDashboard";
 export default function Portfolio() {
   return (
     <>
-            <SEO
-            title="Portfolio | Sunset Ventures"
-            description="Portfolio Sunset Ventures. Quantitative Trading. SEC Approved. Cryptocurrency Trading. Smart Algorithms. Smart FX."
-          />
-          <LayoutDashboard>
-          <div className="relative h-full mx-auto text-trueZinc-700 dark:text-trueZinc-100">
-            <div className="relative mt-8 px-4 md:px-10 mx-auto w-full -m-24">
-              <div className="flex flex-wrap">
-                <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
-                  <div>
-                   Your Portfolio
-                  </div>
-                </div>
-                <div className="w-full xl:w-4/12 px-4">
-                
-                </div>
-              </div>
-              <div className="flex flex-wrap mt-4">
-                <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
-                
-                </div>
-                <div className="w-full xl:w-4/12 px-4">
-                
-                </div>
-              </div>
-            </div>
-          </div>
-        </LayoutDashboard>
-        </>
+      <SEO
+        title="Portfolio | Sunset Ventures"
+        description="Portfolio Sunset Ventures. Quantitative Trading. SEC Approved. Cryptocurrency Trading. Smart Algorithms. Smart FX."
+      />
+      <LayoutDashboard>
+        <div className="relative mx-auto text-trueZinc-700 dark:text-trueZinc-100">
+          <Subscriptions />
+        </div>
+      </LayoutDashboard>
+    </>
   );
-} 
+}
