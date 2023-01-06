35d86a2605867a859080a0365ba3646a804c91c7
Aloysius Teo <aloysiusteo72@gmail.com>
2023-01-06 21:37:30 +0000

    Update 7 files

--- components/cta.js
+++ components/cta.js
@@ -5,7 +5,7 @@ import Container from "./container";
 export default function Cta() {
   return (
     <Container>
-      <div className="flex flex-wrap items-center justify-between w-full max-w-4xl gap-5 mx-auto text-trueZinc-700 dark:text-trueZinc-100 bg-trueZinc-100 dark:bg-trueZinc-800 px-7 py-7 lg:px-12 lg:py-12 lg:flex-nowrap rounded-xl">
+      <div className="flex flex-wrap items-center justify-between w-full mx-auto text-trueZinc-700 dark:text-trueZinc-100 bg-trueZinc-100 dark:bg-trueZinc-800 px-7 py-7 lg:px-12 lg:py-12 lg:flex-nowrap rounded-xl">
         <div className="flex-grow text-center lg:text-left">
           <h2 className="text-2xl font-medium lg:text-3xl text-trueZinc-700 dark:text-trueZinc-100">
             Ready to explore our platform?
